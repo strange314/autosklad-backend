42b7486c3484877863f650de84a1ba97154b6718
izvenyaisya <pankin.nkt@gmail.com>
2026-01-31 19:20:00 +0300

    Remove dist and package-lock from git; add to .gitignore

--- dist/parts/dto/update-part.dto.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.UpdatePartDto = void 0;
-const mapped_types_1 = require("@nestjs/mapped-types");
-const create_part_dto_1 = require("./create-part.dto");
-class UpdatePartDto extends (0, mapped_types_1.PartialType)(create_part_dto_1.CreatePartDto) {
-}
-exports.UpdatePartDto = UpdatePartDto;
-//# sourceMappingURL=update-part.dto.js.map
